596390b05d7c93372fa2bdd78c8475cfe54c9d3f
Abbi White <amwhite1@live.unc.edu>
2021-09-08 14:51:32 -0400

    ex02 again

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -3,12 +3,12 @@
 __author__ = "730281821"
 
 
-beat: str = str(input("What beat do you want to repeat? "))
-repeat: int = int(input("How many times do you want to repeat it? "))
+beat: str = str(input("What beat do you want to repeat?"))
+repeat: int = int(input("How many times do you want to repeat it?"))
 if 0 < repeat:
     song: str = (beat+ " ")*repeat
     print(str(song))
-    repeat = repeat - repeat
+    repeat = repeat + 1
 else:
     repeat < 1
     print("No beat...")
