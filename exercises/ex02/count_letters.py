42640a2517f0510800b1f773a4fbbf45e2a0958b
Abbi White <amwhite1@live.unc.edu>
2021-09-08 15:05:35 -0400

    ex02 again pt 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -5,11 +5,12 @@ __author__ = "730281821"
 letter: str = str(input("What letter do you want to search for? "))
 
 word: str = str(input("Enter a word: "))
-
+i = 0
 count = 0
 
-for i in range(len(word)):
+while(i < len(word)):
     if(word[i] == letter):
         count = count + 1
+    i = i + 1
 
 print("Count: ", count)
