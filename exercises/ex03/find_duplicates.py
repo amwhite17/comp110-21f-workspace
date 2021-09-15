448ef4899437d836dd2f79e13b891c32e6c9cb43
Abbi White <amwhite1@live.unc.edu>
2021-09-14 21:49:18 -0400

    ex03 progress

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,22 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730281821"
+
+word: str = str(input("Enter a word: "))
+bool = False
+
+i = 0
+
+while(i < len(word)):
+    z = word[i]
+    i = i + 1
+    a = i
+    while(a < len(word)):
+        y = word[a]
+        if z == y:
+            bool = True
+        a = a + 1
+
+print("Found duplicate:", bool)
+
+
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,19 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730281821"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+Depth: int = int(input("Depth: "))
+i = 0
+x = ''
+while (i < Depth):
+    i = i + 1
+    z = 0
+    while (z < i):
+        x = x + TREE
+        z = z + 1
+    print(x)
+    temp = ''
+    
