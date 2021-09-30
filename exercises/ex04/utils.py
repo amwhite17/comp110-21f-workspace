879645a16bff1820df9fa6852a37dd05fa0c7ae0
Abbi White <amwhite1@live.unc.edu>
2021-09-29 22:03:56 -0400

    ex04 pt 2

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,47 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730281821"
+
+from random import randint 
 
 
-# TODO: Implement your functions here.
+def all(a: list[int], b: int) -> bool:
+    """The main funciton for all."""
+    i = 0
+    while i < len(a):
+        if a[i] != b:
+            return False 
+        i = i + 1
+    return True
+
+
+print(all)
+
+
+def is_equal(a: list[int], b: list[int]) -> bool:
+    """The Main Function for is_equal."""
+    i = 0
+    while i < len(a):
+        if a[i] != b[i]:
+            return False 
+    i = i + 1
+    return True
+
+
+print(is_equal)
+
+
+def max(a: list[int]) -> int:
+    """The main function for max."""
+    i = 0
+    the_max = a[i]
+    if len(a) == 0:
+        raise ValueError("max() arg is an empty List")
+    while i < len(a):
+        if a[i] > the_max:
+            the_max = a[i]
+        i = i + 1
+    return the_max 
+
+    
+print(max)
